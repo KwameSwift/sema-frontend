ae6bd40d58f3a1d51aabbff7691b6a02769c28ce
Faisal Issaka <117360483+faisal-issaka@users.noreply.github.com>
2023-09-06 22:11:07 +0000

    card styling added

--- src/Screens/Forum/SingleForum/components/forumCard.js
+++ src/Screens/Forum/SingleForum/components/forumCard.js
@@ -36,15 +36,17 @@ function AdminForumCard(props) {
     }
 
     return (
-        <div className="admin-forum-card p-4" onClick={() => navigate(`/forums/${props.id}`)}>
-            <div className="flex justify-between items-center">
-                <div className="flex items-center">
-                    <img
-                        src={props.author__profile_image ? props.author__profile_image : Avatar}
-                        alt=""
-                        className="w-[40px] h-[40px] rounded-full"
-                    />
-                    <span className="flex flex-col ml-3">
+        <div className="admin-forum-card flex flex-col justify-between p-4"
+             onClick={() => navigate(`/forums/${props.id}`)}>
+            <div>
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center">
+                        <img
+                            src={props.author__profile_image ? props.author__profile_image : Avatar}
+                            alt=""
+                            className="w-[40px] h-[40px] rounded-full"
+                        />
+                        <span className="flex flex-col ml-3">
             <span className="author flex justify-between items-center">
               <span>
                 {props.author__first_name} {props.author__last_name}
@@ -56,34 +58,37 @@ function AdminForumCard(props) {
 
             <span className="text-[13px]">{formatDate(props?.created_on)}</span>
           </span>
+                    </div>
+                    <div className="flex items-center forum-type">
+                        <GoDotFill fill="#3e6d9c"/>
+                        <span>{props?.is_public ? "Public" : "Private"}</span>
+                    </div>
                 </div>
-                <div className="flex items-center forum-type">
-                    <GoDotFill fill="#3e6d9c"/>
-                    <span>{props?.is_public ? "Public" : "Private"}</span>
-                </div>
-            </div>
-            <div className="mt-4">
-                <h3 className="font-bold mb-3 text-[20px] title">{props.topic}</h3>
-                <p className="mt-3 mb-2 forum-desc">{props.description}</p>
-                <div className="forum-tags">
-                    {props?.tags?.map((elt, index) => (<span key={index} style={{backgroundColor: tagColors[elt]}}>
+                <div className="mt-4">
+                    <h3 className="font-bold mb-3 text-[20px] title">{props.topic}</h3>
+                    <p className="mt-3 mb-2 forum-desc">{props.description}</p>
+                    <div className="forum-tags">
+                        {props?.tags?.map((elt, index) => (<span key={index} style={{backgroundColor: tagColors[elt]}}>
               {elt}
             </span>))}
+                    </div>
                 </div>
             </div>
-            <hr className="mt-3"/>
-            <div className="flex justify-between items-center">
-                <div className="flex mt-3 items-center justify-between">
-                    {props.is_approved && (<span className="text-[#e14d2a] text-[14px] cursor-default">
+            <div>
+                <hr className="mt-3"/>
+                <div className="flex justify-between items-center">
+                    <div className="flex mt-3 items-center justify-between">
+                        {props.is_approved && (<span className="text-[#e14d2a] text-[14px] cursor-default">
                   <BiSolidChat/> {props?.chat_rooms?.length}
                 </span>
-                    )}
-                </div>
-                <div className="flex mt-3 items-center justify-between status">
-                    <p>{props.is_member
-                        ? <span>Joined</span>
-                        : <span><u>Join now</u></span>}
-                    </p>
+                        )}
+                    </div>
+                    <div className="flex mt-3 items-center justify-between status">
+                        <p>{props.is_member
+                            ? <span>Joined</span>
+                            : <span><u>Join now</u></span>}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>);
