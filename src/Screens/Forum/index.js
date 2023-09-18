faecba2b54dc558168bc4318a8f46063bafc13c8
Faisal Issaka <117360483+faisal-issaka@users.noreply.github.com>
2023-09-18 20:08:23 +0000

    forum not showing fixed

--- src/Screens/Forum/index.js
+++ src/Screens/Forum/index.js
@@ -5,36 +5,39 @@ import {axiosClient, axiosClientWithHeaders} from '../../libs/axiosClient';
 import ForumCard from "./SingleForum/components/forumCard";
 import {useSelector} from "react-redux";
 import "./style.scss";
+import Pagination from "../../Components/Common/Pagination";
+import {BsFillArrowUpCircleFill} from "react-icons/bs";
 
 function ForumsPage() {
     const [forums, setForums] = useState([]);
-    const [category, setCategory] = useState("All");
+    const [currentPage, setCurrentPage] = useState(1);
+    const [forumType, setForumType] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
     const user = useSelector((store) => store.user.tokens);
     const [refetch,] = useState(false);
 
     const elementRef = useRef(null);
 
-    // const scrollToElement = () => {
-    //     if (elementRef.current) {
-    //         elementRef.current.scrollIntoView({behavior: 'smooth'});
-    //     }
-    // };
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
+
+    const getAllForums = async () => {
+        try {
+            let resp = null;
+            if (user.access) {
+                resp = await axiosClientWithHeaders.get(`/forum/get-all-forums/${forumType}/${currentPage}/`);
+            } else {
+                resp = await axiosClient.get(`/forum/get-all-forums/${forumType}/${currentPage}/`);
+            }
+            setForums(resp.data.data);
+            setTotalPages(resp.data.total_pages);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     useEffect(() => {
-        const getAllForums = async () => {
-            try {
-                let resp = null;
-                if (user.access) {
-                    resp = await axiosClientWithHeaders.get(`/forum/get-all-forums/1/`);
-                } else {
-                    resp = await axiosClient.get(`/forum/get-all-forums/1/`);
-                }
-                setForums(resp.data.data);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-
         getAllForums();
     }, [refetch]);
 
@@ -48,46 +51,40 @@ function ForumsPage() {
                         <div className="pt-10 sidebar-menu">
                             <ul>
                                 <li>Menu</li>
-                                <li className={category === "All" && "selected"}
-                                    onClick={() => setCategory("All")}>All
+                                <li className={forumType === 1 && "selected"}
+                                    onClick={() => setForumType(1)}>All
                                 </li>
-                                <li className={category === "Public" && "selected"}
-                                    onClick={() => setCategory("Public")}>Public
+                                <li className={forumType === 2 && "selected"}
+                                    onClick={() => setForumType(2)}>Public
                                 </li>
-                                <li className={category === "Private" && "selected"}
-                                    onClick={() => setCategory("Private")}>Private
+                                <li className={forumType === 3 && "selected"}
+                                    onClick={() => setForumType(3)}>Private
                                 </li>
                             </ul>
                         </div>
                     </aside>
                     <main>
-                        <div className='flex flex-wrap justify-center pt-8' ref={elementRef}>
+                        <div className='flex flex-wrap justify-center pt-8'>
                             {forums.map((elt, index) =>
                                 <ForumCard {...elt} key={index}/>
                             )}
                         </div>
+                        <Pagination
+                            getData={getAllForums}
+                            currentPage={currentPage}
+                            setCurrentPage={setCurrentPage}
+                            totalPages={totalPages}
+                            ref={elementRef}
+                        />
                     </main>
-                    <aside>
-                        <div className="pt-12">
-                            <ul>
-                                <li>Menu</li>
-                            </ul>
-                        </div>
-                    </aside>
                 </div>
-
-                {/*<div className="feeds-content pb-3 pt-8 px-4 w-full flex bg-gray-200" ref={elementRef}>*/}
-                {/*    <div className="feeds-sidebar flex relative top-0 left-0 bottom-0">*/}
-                {/*        <ul className="bg-white w-full">*/}
-                {/*            <li>Blogs</li>*/}
-                {/*            <li>Polls</li>*/}
-                {/*        </ul>*/}
-                {/*    </div>*/}
-                {/*    <div className="bg-white content">*/}
-                {/*        <h1>hy</h1>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
             </div>
+            <BsFillArrowUpCircleFill
+                size={40}
+                fill="#001253"
+                className="fixed right-10 bottom-10 cursor-pointer"
+                onClick={scrollToTop}
+            />
         </div>
     );
 }
